ff8274c83f4de4cd9e5b961a4a19168fa77dd43f
NitroGitRepo <rajneesh.yadav.aws@gmail.com>
2022-09-11 13:08:08 +0530

    second commit

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class index extends Component {
+  render() {
+    return (
+       <>
+         <h2>Types Of Component</h2>
+       </>
+    )
+  }
+}
